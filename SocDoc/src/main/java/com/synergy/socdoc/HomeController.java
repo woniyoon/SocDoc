c17dc35e9f8b38d8fec007467115ac5b8af5e9a4
kwakjiwon <41565055+gold37@users.noreply.github.com>
2020-08-03 20:27:51 +0900

    Change URL pattern

--- SocDoc/src/main/java/com/synergy/socdoc/HomeController.java
+++ SocDoc/src/main/java/com/synergy/socdoc/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/index", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+	@RequestMapping(value = "/index.sd", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
 	public String home(HttpServletRequest request) {
 //		logger.info("Welcome home! The client locale is {}.", locale);
 //		
