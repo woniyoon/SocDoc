b17549874370e1acc408846037989a76ba3c5f74
woniyoon <gellifisch@gmail.com>
2020-08-05 17:03:34 +0900

    Add comments in HpPanelController

--- SocDoc/src/main/java/com/synergy/socdoc/hpPanel/ctrl/HpPanelController.java
+++ SocDoc/src/main/java/com/synergy/socdoc/hpPanel/ctrl/HpPanelController.java
@@ -14,36 +14,43 @@ import com.synergy.service.InterService;
 @Controller
 public class HpPanelController {
 	
+	// 병원회원 페이지 
+	// 홈
 	@RequestMapping(value = "/hpPanel/main.sd", method = RequestMethod.GET)
 	public String main(HttpServletRequest request) {
 
 		return "hpPanel/main.tiles4";
 	}
 
+	// 병원정보관리
 	@RequestMapping(value = "/hpPanel/hpInfo.sd", method = RequestMethod.GET)
 	public String hpInfo(HttpServletRequest request) {
 		
 		return "hpPanel/hpInfo.tiles4";
 	}
 
+	// 예약관리
 	@RequestMapping(value = "/hpPanel/reservationInfo.sd", method = RequestMethod.GET)
 	public String reservationInfo(HttpServletRequest request) {
 		
 		return "hpPanel/reservationInfo.tiles4";
 	}
 
+	// 방문고객관리
 	@RequestMapping(value = "/hpPanel/visitorsMng.sd", method = RequestMethod.GET)
 	public String visitorsMng(HttpServletRequest request) {
 		
 		return "hpPanel/visitorsMng.tiles4";
 	}
 	
+	// 후기보기
 	@RequestMapping(value = "/hpPanel/hpReviews.sd", method = RequestMethod.GET)
 	public String hpReviews(HttpServletRequest request) {
 		
 		return "hpPanel/hpReviews.tiles4";
 	}
 
+	// 통계관리
 	@RequestMapping(value = "/hpPanel/hpStats.sd", method = RequestMethod.GET)
 	public String getHpStats(HttpServletRequest request) {
 		
