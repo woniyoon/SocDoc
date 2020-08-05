13b345e5d2559adc2f811877357bdc97b1f8a94e
woniyoon <gellifisch@gmail.com>
2020-08-05 19:26:17 +0900

    Change the path of jsp files

--- SocDoc/src/main/java/com/synergy/socdoc/hpMem/HpMemController.java
+++ SocDoc/src/main/java/com/synergy/socdoc/hpMem/HpMemController.java
@@ -9,52 +9,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.synergy.service.InterService;
+import com.synergy.socdoc.member.HpInfoVO;
 
 @Component
 @Controller
 public class HpMemController {
+	
+	@Autowired
+	private InterHpMemService service;
 	
 	// 병원회원 페이지 
 	// 홈
 	@RequestMapping(value = "/hpPanel/main.sd", method = RequestMethod.GET)
 	public String main(HttpServletRequest request) {
 
-		return "hpPanel/main.tiles4";
+		
+		return "hpMem/main.tiles4";
 	}
 
 	// 병원정보관리
 	@RequestMapping(value = "/hpPanel/hpInfo.sd", method = RequestMethod.GET)
-	public String hpInfo(HttpServletRequest request) {
+	public String test_hpInfo(HttpServletRequest request) {
 		
-		return "hpPanel/hpInfo.tiles4";
+		// TODO: 나중에는 이 부분을 이용해서 병원정보 가져오기
+//		String hpSeq = request.getSession().getAttribute("hpSeq");
+		String hpSeq = "10";
+		
+		HpInfoVO hpInfo = service.getHpInfo(hpSeq);
+		
+		request.setAttribute("hpInfo", hpInfo);
+		return "hpMem/hpInfo.tiles4";
 	}
 
 	// 예약관리
 	@RequestMapping(value = "/hpPanel/reservationInfo.sd", method = RequestMethod.GET)
 	public String reservationInfo(HttpServletRequest request) {
 		
-		return "hpPanel/reservationInfo.tiles4";
+		return "hpMem/reservationInfo.tiles4";
 	}
 
 	// 방문고객관리
 	@RequestMapping(value = "/hpPanel/visitorsMng.sd", method = RequestMethod.GET)
 	public String visitorsMng(HttpServletRequest request) {
 		
-		return "hpPanel/visitorsMng.tiles4";
+		return "hpMem/visitorsMng.tiles4";
 	}
 	
 	// 후기보기
 	@RequestMapping(value = "/hpPanel/hpReviews.sd", method = RequestMethod.GET)
 	public String hpReviews(HttpServletRequest request) {
 		
-		return "hpPanel/hpReviews.tiles4";
+		return "hpMem/hpReviews.tiles4";
 	}
 
 	// 통계관리
 	@RequestMapping(value = "/hpPanel/hpStats.sd", method = RequestMethod.GET)
 	public String getHpStats(HttpServletRequest request) {
 		
-		return "hpPanel/hpStats.tiles4";
+		return "hpMem/hpStats.tiles4";
 	}
 
 }
